19f2a8d99662c8da94e60b7d767cb76bc0340b72
ginoezue <ginoezue@neusta.de>
2026-02-05 12:05:55 +0100

    Frontend: - Sidebar added

--- DCS/implementation/frontend/ClientApp/src/App.vue
+++ DCS/implementation/frontend/ClientApp/src/App.vue
@@ -5,9 +5,6 @@ import NavBar from './components/NavBar.vue';
 
 <template>
   <header>
-    <div class="bg-blue-500 text-white p-4 rounded">
-      Hallo Tailwind 4 + Vue 3!
-    </div>
     <NavBar />
   </header>
   <main>
--- DCS/implementation/frontend/ClientApp/src/router/router.ts
+++ DCS/implementation/frontend/ClientApp/src/router/router.ts
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import TableView from '../views/TableView.vue'
 import ContractTemplateView from '../views/contract-template/ContractTemplateView.vue'
+import { DocumentTextIcon } from '@heroicons/vue/20/solid'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'Contract Templates',
     component: ContractTemplateView,
+    meta: { icon: DocumentTextIcon }
   },
   {
     path: '/table',
