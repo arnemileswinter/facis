8e3f49edc0cccda0ae694a0c109f60499cb0c0a5
Feifan Lin <l.fin@neusta.de>
2026-02-23 13:22:13 +0100

    feat: block hierarchy indent UI
    Signed-off-by: feifan-lin <l.fin@neusta.de>

--- DCS/implementation/frontend/ClientApp/src/modules/template-repository/components/EditorBlocks.vue
+++ DCS/implementation/frontend/ClientApp/src/modules/template-repository/components/EditorBlocks.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="flex flex-col gap-2">
-    <EditorBlock v-for="item in flatItemsWithBlock" :key="item.blockId" :block-id="item.blockId" :block="item.block"
-      :can-move-up="item.siblingIndex > 0" :can-move-down="item.siblingIndex < item.siblingCount - 1"
-      :can-outdent="item.canOutdent" :can-indent="item.canIndent"
-      @insert-above="openAddBlockModal(item.parentBlockId, item.siblingIndex)"
-      @insert-below="openAddBlockModal(item.parentBlockId, item.siblingIndex + 1)"
-      @insert-nest="openAddBlockModal(item.blockId, 0)" @confirm="(payload) => confirmBlock(item.blockId, payload)"
-      @move-up="moveBlockUp(item.blockId, item.parentBlockId, item.siblingIndex)"
-      @move-down="moveBlockDown(item.blockId, item.parentBlockId, item.siblingIndex)"
-      @move-outdent="moveBlockOutdent(item.blockId, item.outdentGrandparentBlockId, item.outdentInsertIndex)"
-      @move-indent="moveBlockIndent(item.blockId, item.indentParentBlockId, item.indentInsertIndex)"
-      @delete="deleteBlock(item.blockId)" />
+    <div v-for="item in flatItemsWithBlock" :key="item.blockId" class="flex items-stretch min-w-0">
+      <!-- Indent area: width by depth, left border for children to show hierarchy -->
+      <div
+        :class="['flex-shrink-0', 'transition-[width]', 'duration-300', 'ease-out', item.depthLevel > 0 && 'border-l-2 border-base-300']"
+        :style="{ width: indentWidth(item.depthLevel) }" aria-hidden />
+      <EditorBlock :block-id="item.blockId" :block="item.block" :can-move-up="item.siblingIndex > 0"
+        :can-move-down="item.siblingIndex < item.siblingCount - 1" :can-outdent="item.canOutdent"
+        :can-indent="item.canIndent" @insert-above="openAddBlockModal(item.parentBlockId, item.siblingIndex)"
+        @insert-below="openAddBlockModal(item.parentBlockId, item.siblingIndex + 1)"
+        @insert-nest="openAddBlockModal(item.blockId, 0)" @confirm="(payload) => confirmBlock(item.blockId, payload)"
+        @move-up="moveBlockUp(item.blockId, item.parentBlockId, item.siblingIndex)"
+        @move-down="moveBlockDown(item.blockId, item.parentBlockId, item.siblingIndex)"
+        @move-outdent="moveBlockOutdent(item.blockId, item.outdentGrandparentBlockId, item.outdentInsertIndex)"
+        @move-indent="moveBlockIndent(item.blockId, item.indentParentBlockId, item.indentInsertIndex)"
+        @delete="deleteBlock(item.blockId)" />
+    </div>
   </div>
 </template>
 
@@ -96,6 +101,7 @@ function enrichFlatItem(
     siblingIndex: item.siblingIndex,
     siblingCount,
     parentBlockId: item.parentBlockId,
+    depthLevel: item.depthLevel,
     canOutdent,
     canIndent,
     outdentGrandparentBlockId,
@@ -103,5 +109,11 @@ function enrichFlatItem(
     indentParentBlockId: prevSiblingBlockId ?? '',
     indentInsertIndex,
   }
+}
+
+/** Indent width per nesting level (px) */
+const INDENT_PER_LEVEL = 16
+function indentWidth(depth: number): string {
+  return `${depth * INDENT_PER_LEVEL}px`
 }
 </script>
